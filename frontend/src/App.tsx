75f7e6876d53cabc5cbfc941ac106fb275053e5a
OAHstasy <o.anilhicyilmaz@gmail.com>
2025-05-27 13:00:54 +0300

    add routes for creating and viewing user posts with PrivateRoute protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import PrivateRoute from './components/PrivateRoute';
+import CreatePostPage from './pages/CreatePostPage';
+import UserPostsPage from './pages/UserPostsPage';
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/new-post" element={<PrivateRoute><CreatePostPage /></PrivateRoute>} />
+        <Route path="/my-posts" element={<PrivateRoute><UserPostsPage /></PrivateRoute>} />
       </Routes>
     </Router>
   );
